3906e6e59d9740c0541443bb0bed427e91a05aa4
Edouard <edouardbrodeur@orange.fr>
2022-11-10 09:54:54 +0100

    removed printf from export function

--- builtins/functions/builtin_export_unset.c
+++ builtins/functions/builtin_export_unset.c
@@ -6,7 +6,7 @@
 /*   By: ebrodeur <ebrodeur@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:58:35 by ebrodeur          #+#    #+#             */
-/*   Updated: 2022/11/10 09:38:03 by ebrodeur         ###   ########lyon.fr   */
+/*   Updated: 2022/11/10 09:54:38 by ebrodeur         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,12 @@ int	mini_export(t_mini_data *data, char *var)
 		data->p_status = 0;
 		return (0);
 	}
-	printf("Begin export --> %d\n", data->envp_size);
 	data->new_env = malloc(sizeof(char *) * (data->envp_size + 2));//1 pour la var_export et 1 pour le NULL
 	if (!data->new_env)
 		return (1);
 	data->new_env = newtab_malloc(data, data->envp_size + 1, data->env, var);
 	copy_loop(data, var, data->envp_size + 1);
 	data->envp_size++;
-	printf("End export --> %d\n", data->envp_size);
 	data->new_env[data->envp_size] = NULL;
 	data->p_status = 0;
 	return (0);
